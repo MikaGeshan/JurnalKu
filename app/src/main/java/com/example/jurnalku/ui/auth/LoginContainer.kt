1e15c98553ed5a386e1621f1b450cee7e63944da
mika.arip <mika.arip@quadrant-si.id>
2026-04-25 18:26:40 +0700

    added handling sign in with google

--- app/src/main/java/com/example/jurnalku/ui/auth/LoginContainer.kt
+++ app/src/main/java/com/example/jurnalku/ui/auth/LoginContainer.kt
@@ -1,19 +1,23 @@
 package com.example.jurnalku.ui.auth
 
-import android.util.Log
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import com.example.jurnalku.R
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import android.app.Activity.RESULT_OK
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.jurnalku.ui.stores.AuthStore
+import com.google.android.gms.auth.api.signin.*
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
-data class LoginPayload(
-    val email: String,
-    val password: String
-)
 
 @Composable
 fun LoginContainer(
@@ -25,9 +29,11 @@ fun LoginContainer(
     var isLoading by remember { mutableStateOf(false) }
     var errorMessage by remember { mutableStateOf<String?>(null) }
 
+//  Check existing user data
     val auth = remember { FirebaseAuth.getInstance() }
     val authStore: AuthStore = viewModel()
 
+//  handle function login
     fun handleLogin() {
         if (isLoading) return
 
@@ -43,6 +49,7 @@ fun LoginContainer(
             .addOnCompleteListener { task ->
                 isLoading = false
 
+//              success & error validations
                 if (task.isSuccessful) {
                     val user = auth.currentUser
                     Log.d("LOGIN_SUCCESS", "uid=${user?.uid}")
@@ -56,8 +63,64 @@ fun LoginContainer(
             }
     }
 
+    val context = LocalContext.current
+
+    val googleSignInClient = remember {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(context.getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        GoogleSignIn.getClient(context, gso)
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+
+        if (result.resultCode == RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+                val idToken = account.idToken
+
+                if (idToken == null) {
+                    errorMessage = "ID Token null"
+                    return@rememberLauncherForActivityResult
+                }
+
+                val credential = GoogleAuthProvider.getCredential(idToken, null)
+
+                isLoading = true
+
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener { task ->
+
+                        isLoading = false
+
+                        if (task.isSuccessful) {
+                            authStore.refreshUser()
+                            onLoginSuccess()
+                        } else {
+                            errorMessage = "Google login gagal"
+                        }
+                    }
+
+            } catch (e: Exception) {
+                errorMessage = task.exception?.message ?: "Google login gagal"
+                Log.e("GOOGLE_LOGIN_ERROR", errorMessage!!)
+            }
+        }
+    }
+
+
+//    handle login with google
     fun handleGoogleLogin() {
-        // next step
+        if (isLoading) return
+
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
     }
 
     LoginScreen(
